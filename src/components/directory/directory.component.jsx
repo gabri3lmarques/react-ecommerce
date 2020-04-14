8cd51ed2fbda19cd48d393b4d31b3ed7871464e7
Gabriel <gabri3lmarques@gmail.com>
2020-04-14 17:32:19 -0300

    criação dos componentes Directorye MenuItem

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./directory.style.scss";
+
+const Directory = () => {
+  return (
+    <div className="directory">
+      <h1>Directory</h1>
+    </div>
+  );
+};
+
+export default Directory;
